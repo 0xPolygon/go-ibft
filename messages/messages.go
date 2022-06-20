d665c15071e704b7e6d242ca26d939c0f1c5f934
dbrajovic <dbrajovic3@gmail.com>
2022-06-20 11:03:41 +0200

    simplify pointer

--- messages/messages.go
+++ messages/messages.go
@@ -1,15 +1,16 @@
 package messages
 
 import (
-	"github.com/Trapesys/go-ibft/proto"
 	"sync"
+
+	"github.com/Trapesys/go-ibft/proto"
 )
 
 // heightMessageMap maps the height number -> round message map
-type heightMessageMap map[uint64]*roundMessageMap
+type heightMessageMap map[uint64]roundMessageMap
 
 // roundMessageMap maps the round number -> messages
-type roundMessageMap map[uint64]*protoMessages
+type roundMessageMap map[uint64]protoMessages
 
 // protoMessages is the set of messages that circulate.
 // It contains a mapping between the sender and their messages to avoid duplicates
@@ -21,29 +22,32 @@ type Messages struct {
 	sync.RWMutex
 
 	// message maps for different message types
-	preprepareMessageMap  *heightMessageMap
-	prepareMessageMap     *heightMessageMap
-	commitMessageMap      *heightMessageMap
-	roundChangeMessageMap *heightMessageMap
+	preprepareMessages,
+	prepareMessages,
+	commitMessages,
+	roundChangeMessages heightMessageMap
 }
 
-func (h *heightMessageMap) getViewMessages(view *proto.View) *protoMessages {
-	// Check if the height is present
-	roundMessages, exists := (*h)[view.Height]
-	if !exists {
-		roundMessages = &roundMessageMap{
-			view.Round: &protoMessages{},
-		}
+func (m heightMessageMap) getViewMessages(view *proto.View) protoMessages {
+	var (
+		height = view.Height
+		round  = view.Round
+	)
 
-		(*h)[view.Height] = roundMessages
+	// Check if the height is present
+	roundMessages, exists := m[height]
+	if !exists {
+		roundMessages = roundMessageMap{round: protoMessages{}}
+
+		m[height] = roundMessages
 	}
 
 	// Check if the round is present
-	messages, exists := (*roundMessages)[view.Round]
+	messages, exists := roundMessages[round]
 	if !exists {
-		messages = &protoMessages{}
+		messages = protoMessages{}
 
-		(*roundMessages)[view.Round] = messages
+		roundMessages[round] = messages
 	}
 
 	return messages
@@ -52,24 +56,24 @@ func (h *heightMessageMap) getViewMessages(view *proto.View) *protoMessages {
 // NewMessages returns a new Messages wrapper
 func NewMessages() *Messages {
 	return &Messages{
-		preprepareMessageMap:  &heightMessageMap{},
-		prepareMessageMap:     &heightMessageMap{},
-		commitMessageMap:      &heightMessageMap{},
-		roundChangeMessageMap: &heightMessageMap{},
+		preprepareMessages:  make(heightMessageMap),
+		prepareMessages:     make(heightMessageMap),
+		commitMessages:      make(heightMessageMap),
+		roundChangeMessages: make(heightMessageMap),
 	}
 }
 
 // getMessageMap fetches the corresponding message map by type
-func (ms *Messages) getMessageMap(messageType proto.MessageType) *heightMessageMap {
+func (ms *Messages) getMessageMap(messageType proto.MessageType) heightMessageMap {
 	switch messageType {
 	case proto.MessageType_PREPREPARE:
-		return ms.preprepareMessageMap
+		return ms.preprepareMessages
 	case proto.MessageType_PREPARE:
-		return ms.prepareMessageMap
+		return ms.prepareMessages
 	case proto.MessageType_COMMIT:
-		return ms.commitMessageMap
+		return ms.commitMessages
 	case proto.MessageType_ROUND_CHANGE:
-		return ms.roundChangeMessageMap
+		return ms.roundChangeMessages
 	}
 
 	return nil
@@ -85,7 +89,7 @@ func (ms *Messages) AddMessage(message *proto.Message) {
 
 	// Append the message to the appropriate queue
 	messages := heightMsgMap.getViewMessages(message.View)
-	(*messages)[string(message.From)] = message
+	messages[string(message.From)] = message
 }
 
 // NumMessages returns the number of messages received for the specific type
@@ -99,18 +103,18 @@ func (ms *Messages) NumMessages(
 	heightMsgMap := ms.getMessageMap(messageType)
 
 	// Check if the round map is present
-	roundMsgMap, found := (*heightMsgMap)[view.Height]
+	roundMsgMap, found := heightMsgMap[view.Height]
 	if !found {
 		return 0
 	}
 
 	// Check if the messages array is present
-	messages, found := (*roundMsgMap)[view.Round]
+	messages, found := roundMsgMap[view.Round]
 	if !found {
 		return 0
 	}
 
-	return len(*messages)
+	return len(messages)
 }
 
 // PruneByHeight prunes out all old messages from the message queues
@@ -133,7 +137,7 @@ func (ms *Messages) PruneByHeight(view *proto.View) {
 		// Delete all height maps up until and including the specified
 		// view height
 		for height := uint64(0); height <= view.Height; height++ {
-			delete(*messageMap, height)
+			delete(messageMap, height)
 		}
 	}
 }
@@ -155,12 +159,12 @@ func (ms *Messages) PruneByRound(view *proto.View) {
 	for _, messageType := range possibleMaps {
 		typeMap := ms.getMessageMap(messageType)
 
-		heightMap, exists := (*typeMap)[view.Height]
+		heightMap, exists := typeMap[view.Height]
 		if exists {
 			// Delete all round maps up until and including the specified
 			// view round
 			for round := uint64(0); round <= view.Round; round++ {
-				delete(*heightMap, round)
+				delete(heightMap, round)
 			}
 		}
 	}
