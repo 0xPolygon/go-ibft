e9051df7109d8c68fb53d827eba5e1f81c49690e
Miloš Živković <milos.zivkovic@mvpworkshop.co>
2022-06-21 10:41:00 +0200

    Backend interface (#3)
    Set up the base Backend interface

---
+++ backend/backend.go
@@ -0,0 +1,25 @@
+package backend
+
+import "github.com/Trapesys/go-ibft/proto"
+
+// Backend defines an interface all backend implementations
+// need to implement
+type Backend interface {
+	// IsValidBlock checks if the proposed block is child of parent
+	IsValidBlock(block []byte) bool
+
+	// IsValidMessage checks if signature is from sender
+	IsValidMessage(msg *proto.Message) bool
+
+	// IsProposer checks if the passed in ID is the Proposer for current view (sequence, round)
+	IsProposer(id []byte, sequence, round uint64) bool
+
+	// BuildProposal builds a new block proposal
+	BuildProposal(blockNumber uint64) ([]byte, error)
+
+	// VerifyProposalHash checks if the hash matches the proposal
+	VerifyProposalHash(proposal, hash []byte) error
+
+	// IsValidCommittedSeal checks if the seal for the proposal is valid
+	IsValidCommittedSeal(proposal, seal []byte) bool
+}
