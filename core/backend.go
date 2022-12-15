b0d7db6f255e8deb668834c1d415ae2aea89f2ba
kourin <kourin.code@gmail.com>
2022-12-15 16:49:28 +0900

    Add supplement of comments for backend

--- core/backend.go
+++ core/backend.go
@@ -6,21 +6,24 @@ import (
 )
 
 // MessageConstructor defines a message constructor interface
+// All constructed messages must be signed by a validator for the whole message
 type MessageConstructor interface {
-	// BuildPrePrepareMessage builds a PREPREPARE message based on the passed in proposal
+	// BuildPrePrepareMessage builds a PREPREPARE message based on the passed in view and proposal
 	BuildPrePrepareMessage(
 		proposal []byte,
 		certificate *proto.RoundChangeCertificate,
 		view *proto.View,
 	) *proto.Message
 
-	// BuildPrepareMessage builds a PREPARE message based on the passed in proposal
+	// BuildPrepareMessage builds a PREPARE message based on the passed in view and proposal hash
 	BuildPrepareMessage(proposalHash []byte, view *proto.View) *proto.Message
 
-	// BuildCommitMessage builds a COMMIT message based on the passed in proposal
+	// BuildCommitMessage builds a COMMIT message based on the passed in view and proposal hash
+	// Must create a committed seal for proposal hash and include into the message
 	BuildCommitMessage(proposalHash []byte, view *proto.View) *proto.Message
 
-	// BuildRoundChangeMessage builds a ROUND_CHANGE message based on the passed in proposal
+	// BuildRoundChangeMessage builds a ROUND_CHANGE message based on the passed in view,
+	// latest prepared proposed block, and latest prepared certificate
 	BuildRoundChangeMessage(
 		proposal []byte,
 		certificate *proto.PreparedCertificate,
@@ -30,10 +33,13 @@ type MessageConstructor interface {
 
 // Verifier defines the verifier interface
 type Verifier interface {
-	// IsValidBlock checks if the proposed block is child of parent
+	// IsValidBlock checks if the proposed block is valid and child of the latest block in local chain
 	IsValidBlock(block []byte) bool
 
-	// IsValidSender checks if signature is from sender
+	// IsValidValidator checks if a signature in message js signed by sender
+	// Must check the following things:
+	// (1) recover the signature and the signer matches from address in message
+	// (2) the signer address is one of the validators at the height in message
 	IsValidSender(msg *proto.Message) bool
 
 	// IsProposer checks if the passed in ID is the Proposer for current view (sequence, round)
@@ -42,8 +48,9 @@ type Verifier interface {
 	// IsValidProposalHash checks if the hash matches the proposal
 	IsValidProposalHash(proposal, hash []byte) bool
 
-	// IsValidCommittedSeal checks if the seal for the proposal is valid
-	IsValidCommittedSeal(proposal []byte, committedSeal *messages.CommittedSeal) bool
+	// IsValidCommittedSeal checks
+	// if signature for proposal hash in committed seal is signed by a validator
+	IsValidCommittedSeal(proposalHash []byte, committedSeal *messages.CommittedSeal) bool
 }
 
 // Backend defines an interface all backend implementations
