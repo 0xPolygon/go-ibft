2346e874609833f999d60b937eb03b9357fd6f51
Nemanja0x <nemanja@ethernal.tech>
2023-07-04 11:18:41 +0200

    main fixed

--- build/main.go
+++ build/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	b := core.NewIBFT(nil, nil, nil, nil)
+	b := core.NewIBFT(nil, nil, nil)
 
 	// prevent golang compiler from removing the whole function
 	_, _ = fmt.Fprint(io.Discard, b)
